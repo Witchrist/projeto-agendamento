bf8c5717f8ec3bcc842068f5a75385ac6eae5146
Christopher Alexander <christopherqueiroz@hotmail.com>
2022-10-04 00:17:27 -0300

    primeiro commit

---
+++ projeto-agendamento-backend/src/test/java/br/com/tokiomarine/projetoagendamentobackend/ProjetoAgendamentoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tokiomarine.projetoagendamentobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAgendamentoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
